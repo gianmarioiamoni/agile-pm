496e59d670055680cbec1621088c78385327ac47
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-04-30 21:12:07 +0200

    add schema for team members assignment to projects

---
+++ server/models/assignment.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const assignmentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
+    roleId: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+const Assignment = mongoose.model('Assignment', assignmentSchema);
+
+export default Assignment;
+
+
